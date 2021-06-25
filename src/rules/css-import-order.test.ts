7c4e87fc9f541535c75692593160713a9c68b54d
Andrew <Andrew5282011@gmail.com>
2021-06-25 14:31:43 +0300

    Fix case where multiple css imports are grouped

--- src/rules/css-import-order.test.ts
+++ src/rules/css-import-order.test.ts
@@ -4,32 +4,89 @@ import rule from "./css-import-order";
 
 const tester = new RuleTester({ parserOptions: { ecmaVersion: 2015, sourceType: "module" } });
 
+const newLineBeforeError = { message: "Expected a new line before the css import statement" };
+const endOfTheInputBlockError = { message: "Expected the css import statement to be in the end of the import block" };
+
 tester.run("css-import-order", rule, {
   valid: [{ code:
 `import React from "react";
 import localFile from "../local/file";
 
 import "./local-css-file.scss";`,
-  }],
+  },
+    { code:
+        `import React from "react";
+import localFile from "../local/file";
+
+import "./local-css-file.scss";
+import "./other-local-css-file.scss";`,
+    },
+    { code:
+        `import "./local-css-file.scss";
+import "./other-local-css-file.scss";
+import "./one-more-local-css-file.scss";`,
+    }],
   invalid: [
+    { code:
+        `import "./local-css-file.scss";
+import "./other-local-css-file.scss";
+import "./one-more-local-css-file.scss";
+import React from "react";`,
+      options: [{ applyAllFixes: true }],
+      errors: [endOfTheInputBlockError, endOfTheInputBlockError, endOfTheInputBlockError],
+      output:
+        `import "./other-local-css-file.scss";
+import "./one-more-local-css-file.scss";
+import React from "react";
+
+import "./local-css-file.scss";`,
+    },
+    { code:
+        `import "./other-local-css-file.scss";
+import "./one-more-local-css-file.scss";
+import React from "react";
+
+import "./local-css-file.scss";`,
+      errors: [endOfTheInputBlockError, endOfTheInputBlockError],
+      output:
+        `import "./one-more-local-css-file.scss";
+import React from "react";
+
+import "./local-css-file.scss";
+import "./other-local-css-file.scss";`,
+    },
+    { code:
+        `import "./one-more-local-css-file.scss";
+import React from "react";
+
+import "./local-css-file.scss";
+import "./other-local-css-file.scss";`,
+      errors: [endOfTheInputBlockError],
+      output:
+        `import React from "react";
+
+import "./local-css-file.scss";
+import "./other-local-css-file.scss";
+import "./one-more-local-css-file.scss";`,
+    },
     {
       code:
 `import React from "react";
 import localFile from "../local/file";
 import "./local-css-file.scss";`,
-      errors: [{ message: "Expected a new line before the css import statement" }],
+      errors: [newLineBeforeError],
       output:
 `import React from "react";
 import localFile from "../local/file";
 
-import "./local-css-file.scss";`
+import "./local-css-file.scss";`,
     },
     {
       code:
 `import "./local-css-file.scss";
 import React from "react";
 import localFile from "../local/file";`,
-      errors: [{ message: "Expected the css import statement to be in the end of the import block" }],
+      errors: [endOfTheInputBlockError],
       output:
 `import React from "react";
 import localFile from "../local/file";
@@ -42,7 +99,7 @@ import "./local-css-file.scss";`
 
 import "./local-css-file.scss";
 import localFile from "../local/file";`,
-      errors: [{ message: "Expected the css import statement to be in the end of the import block" }],
+      errors: [endOfTheInputBlockError],
       output:
         `import React from "react";
 import localFile from "../local/file";
@@ -54,7 +111,7 @@ import "./local-css-file.scss";`
         `import React from "react";
 import "./local-css-file.scss";
 import localFile from "../local/file";`,
-      errors: [{ message: "Expected the css import statement to be in the end of the import block" }],
+      errors: [endOfTheInputBlockError],
       output:
         `import React from "react";
 import localFile from "../local/file";
@@ -67,13 +124,13 @@ import "./local-css-file.scss";`
 import "./local-css-file.scss";
 
 import localFile from "../local/file";`,
-      errors: [{ message: "Expected the css import statement to be in the end of the import block" }],
+      errors: [endOfTheInputBlockError],
       output:
         `import React from "react";
 
 import localFile from "../local/file";
 
-import "./local-css-file.scss";`
+import "./local-css-file.scss";`,
     },
   ],
 });
--- src/rules/css-import-order.ts
+++ src/rules/css-import-order.ts
@@ -15,75 +15,90 @@ const rule: Rule.RuleModule = {
       },
       "Program:exit": () => {
         const sourceCode = context.getSourceCode();
-        const cssImport = importStatements.find((imp) => /\.s?css$/.test(imp.source.value as string));
+        const checkForCssImport = (str: string) => /\.s?css/.test(str);
+        const cssImports = importStatements.filter((imp) => checkForCssImport(imp.source.value as string));
 
         const lastImport = importStatements[importStatements.length - 1];
+        const lastNonCssImport = importStatements.filter((imp) => !checkForCssImport(imp.source.value as string)).reverse()[0];
 
-        if (!cssImport) {
+        if (!cssImports.length || !lastNonCssImport) {
           return;
         }
+
+        const lastNonCssImportRange = lastNonCssImport.range as AST.Range;
 
         const lines = sourceCode.getLines();
-        const currentLineNumber = cssImport.source.loc?.start.line;
 
-        if (!currentLineNumber || currentLineNumber < 0) {
-          return;
-        }
+        cssImports.forEach((cssImport) => {
+          const currentLineNumber = cssImport.source.loc?.start.line;
+          const lastNonCssImportLineNumber = lastNonCssImport.source.loc?.start.line;
 
-        const currentLineIndex = currentLineNumber - 1;
-        const lineBeforeCssImport = lines[currentLineIndex - 1];
-        // if this is the first line isLineBeforeCssImportEmpty should be false
-        const isLineBeforeCssImportEmpty = lineBeforeCssImport != undefined ? /^\s*$/g.test(lineBeforeCssImport) : false;
-
-        // if the css import is the last import already
-        if (importStatements.indexOf(lastImport) === importStatements.indexOf(cssImport)) {
-          // insert an empty line if missing
-          if (!isLineBeforeCssImportEmpty) {
-            context.report({
-              node: cssImport,
-              message: "Expected a new line before the css import statement",
-              fix(fixer) {
-                return fixer.insertTextBefore(cssImport, "\n");
-              },
-            });
-          }
-          return;
-        }
-
-        const cssImportRange = cssImport.range as AST.Range;
-
-        const cssImportCode = "\n\n" + sourceCode.text.slice(...cssImportRange);
-
-        const oldCssImportRange = (() => {
-          // if the first line
-          if (currentLineIndex === 0) {
-            // remove with the \n in the end of line
-            return [0, cssImportRange[1] + 1] as AST.Range;
+          if (!currentLineNumber || currentLineNumber < 0 || !lastNonCssImportLineNumber) {
+            return;
           }
 
-          // if there's an empty line before
-          if (isLineBeforeCssImportEmpty) {
-            // remove with the \n\n before the line
-            return [cssImportRange[0] - 2, cssImportRange[1]] as AST.Range;
+          const currentLineIndex = currentLineNumber - 1;
+          const lineBeforeCssImport = lines[currentLineIndex - 1];
+          // if this is the first line isLineBeforeCssImportEmpty should be false
+          const isLineBeforeCssImportEmpty = lineBeforeCssImport != undefined ? /^\s*$/g.test(lineBeforeCssImport) : false;
+          const isLineBeforeCssImportIsCssImport = lineBeforeCssImport && checkForCssImport(lineBeforeCssImport);
+
+          // if the css import is below the last non css import and there's a blank line or other css import above - do nothing
+          if (currentLineNumber > lastNonCssImportLineNumber && (isLineBeforeCssImportEmpty || isLineBeforeCssImportIsCssImport)) {
+            return;
           }
-          // other cases
 
-          // remove with the \n before the line
-          return [cssImportRange[0] - 1, cssImportRange[1]] as AST.Range;
-        })();
+          // if the line before css import is the last non css import
+          if (lineBeforeCssImport && lineBeforeCssImport === sourceCode.text.slice(...lastNonCssImportRange)) {
+            return context.report({
+                node: cssImport,
+                message: "Expected a new line before the css import statement",
+                fix(fixer) {
+                  return fixer.insertTextBefore(cssImport, "\n");
+                },
+              });
+          }
 
-        // if the css import is not the last one - remove the old css and append the new one after the last import
-        context.report({
-          node: cssImport,
-          message: "Expected the css import statement to be in the end of the import block",
-          fix(fixer) {
-            return [
-              // append css import after the last import
-              fixer.insertTextAfter(lastImport, cssImportCode),
-              // stripe out css import from previous place
-              fixer.replaceTextRange(oldCssImportRange, ""),
-            ];
-          },
+          const cssImportRange = cssImport.range as AST.Range;
+
+          const isLastImportIsCssImport = checkForCssImport(lastImport.source.value as string);
+
+          // do not add a blank line before if there's a group of the css imports
+          const padding = isLastImportIsCssImport ? "\n" : "\n\n";
+
+          const cssImportCode = padding + sourceCode.text.slice(...cssImportRange);
+
+          const oldCssImportRange = (() => {
+            // if the first line
+            if (currentLineIndex === 0) {
+              // remove with the \n in the end of line
+              return [0, cssImportRange[1] + 1] as AST.Range;
+            }
+
+            // if there's an empty line before
+            if (isLineBeforeCssImportEmpty) {
+              // remove with the \n\n before the line
+              return [cssImportRange[0] - 2, cssImportRange[1]] as AST.Range;
+            }
+            // other cases
+
+            // remove with the \n before the line
+            return [cssImportRange[0] - 1, cssImportRange[1]] as AST.Range;
+          })();
+
+          // if the css import is not the last one - remove the old css and append the new one after the last import
+          context.report({
+            node: cssImport,
+            message: "Expected the css import statement to be in the end of the import block",
+            fix(fixer) {
+              return [
+                // append css import after the last import
+                fixer.insertTextAfter(lastImport, cssImportCode),
+                // stripe out css import from previous place
+                fixer.replaceTextRange(oldCssImportRange, ""),
+              ];
+            },
+          });
         });
 
         importStatements = [];
